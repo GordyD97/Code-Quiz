29ed2a0d5cf09d4f4e9d0f9bfe9f6dd7e03534eb
Turbo Gordz <gordon.draine97@yahoo.com>
2022-07-17 23:10:36 -0400

    debugging button text and buttons showing up twice

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 # Code-Quiz
+
+## Summary
+```md
+This application is used to test the users skills on the topic of webdevelopement/ coding 
+the user will be prompted with multiple questions that are related to the topic challenging the user to use their coding knowledge so they can get a higher score. The application will also help train the user if they are not very comfortable with the subject. 
+
+```
+
+
+## Task
+```md
+The user will be prompted with multipe a question regarding to coding. The user will then be prommpted with a set of multipe choice answers that will contain one right answer and one wrong answer. The count of right and wrong answers will increase according to the users selections. After the user is done with the quiz they can submit the quiz to review their score. The user will then be prompted with their most previous score, local high scores, and top all time highscore for their device. 
+
+```
+
+## Technologies
+```md
+The application uses technologies such as:
+1. HTML
+2. CSS (Cascading style sheets)
+3. JS (javascript)
+
+```
+
+### Acceptance criteria 
+
+```md
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+### Links
+
+1. 
+2. 
--- assets/javatest2.js
+++ assets/javatest2.js
@@ -1,5 +1,3 @@
-// first state variables
-
 let startBttn = document.getElementById('startBttn');
 let submitbttn = document.getElementById('submitbttn');
 
@@ -37,14 +35,14 @@ var questions = [
 //create a start quiz/ submit quiz button funciton
 // function startquiz();
 // question next will be a call back
-    // dont have to define it now
+// dont have to define it now
 
-const createAnswerButton = (textContent, rightBool, questionNext) =>{
-   var button = document.createElement('button')
-   button.setAttribute('data-correct', rightBool)
-   button.textContent = textContent;
-   button.addEventListener('click', questionNext)
-   return button; 
+const createAnswerButton = (textContent, rightBool, questionNext) => {
+    var button = document.createElement('button')
+    button.setAttribute('data-correct', rightBool)
+    button.textContent = textContent;
+    button.addEventListener('click', questionNext)
+    return button;
 };
 let questionIndex = 0
 const startQuiz = () => {
@@ -54,15 +52,15 @@ const startQuiz = () => {
 
     // creating question container
     const div = document.createElement("div");
-    div.children[2];
-    
+    div.children[0];
+
 
     const questionContainer = document.createElement("h1");
     questionContainer.innerText = questions[0].question;
     // create for loop
     // for(var questions= 0; questions< 0; questions++)
     const answerContainer = document.createElement('p');
-    answerContainer.innerText = questions[0].choices;
+    answerContainer.textcontent = questions[0].choices;
     console.log(questionContainer);
 
 
@@ -70,58 +68,44 @@ const startQuiz = () => {
     document.body.appendChild(answerContainer);
     document.body.appendChild(div);
     // adding answers to answer container
-    for (let i=0; i< questions[questionIndex].choices.length; i++){ 
+    // creating the answer button 
+    for (let i = 0; i < questions[questionIndex].choices.length; i++) {
         let textContent = questions[questionIndex].choices[i]
         let rightBool = questions[questionIndex].answer === i
         let answerButton = createAnswerButton(
-            textContent,rightBool, function(){
+            textContent, rightBool, function () {
                 // if rightBool{
                 //     // add to the right count
                 // }
                 // else
                 console.log(rightBool);
                 console.log(textContent);
-                console.log(questionIndex+1);
+                console.log(questionIndex + 1);
             }
         );
         answerContainer.append(answerButton);
     }
 
-    
-    
+
+
     // crteate element update attributes then append to page
-        // create element tag that will be empty inside of it 
-            // text content* 
-                // each button has to have a specific value/data attribute for right or wrong 
-    
+    // create element tag that will be empty inside of it 
+    // text content* 
+    // each button has to have a specific value/data attribute for right or wrong 
 
 
-//   add question and anwer container
 
-        // console.log('hello test')
-       
-        
+    //   add question and anwer container
+
+    // console.log('hello test')
+
+
 };
 
- 
+
 // next task to acess each one after it is selected
 // iterator ++ after the ansewr is selected 
 
 startBttn.addEventListener('click', startQuiz);
 
-submitQuiz =>{};
-//create a show question function 
-
-//create a function that adds the correct question counter 
-
-// create a function that counts down the timer 
-
-//create a function for saving scores json/local storage
-
-// create a function for brining the next question
-
-// create a function for displaying final score plus leaader board stringify/local storage/json
-
-
-
-//call back all functions 
+submitQuiz => { };
